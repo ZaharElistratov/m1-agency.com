00448e9ec38d98b3d567426883ccf146332ddd2c
Захар Елистратов <104322990+ZaharElistratov@users.noreply.github.com>
2023-11-29 20:27:40 +0400

    Add files via upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,17 +7,17 @@ import GoogleAnalytics from "@/components/GoogleAnalytics";
 const rostelecom = localFont({
     src: [
         {
-            path: './fonts/rostelecomBasis-Regular.woff2',
+            path: '../../public/fonts/RostelecomBasis-Regular.woff2',
             weight: '400',
             style: 'normal'
         },
         {
-            path: './fonts/rostelecomBasis-Medium.woff2',
+            path: '../../public/fonts/RostelecomBasis-Medium.woff2',
             weight: '500',
             style: 'normal'
         },
         {
-            path: './fonts/rostelecomBasis-Bold.woff2',
+            path: '../../public/fonts/RostelecomBasis-Bold.woff2',
             weight: '700',
             style: 'normal'
         },
